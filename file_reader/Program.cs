9a4d52fedfb3daf87f9665ed8583cc34b184f46c
Kunal Krishneel <kunalkrishneel@gmail.com>
2023-09-05 22:05:49 +1200

    file reader proj commit

---
+++ file_reader/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+  string filePath = "data.txt";
+        using (StreamReader reader = new StreamReader(filePath))
+        {
+            string content = await reader.ReadToEndAsync();
+            Console.WriteLine(content);
+        }
